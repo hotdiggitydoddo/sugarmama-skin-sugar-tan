8a89bb84870f7252c6e4e25b0fc11b671c703c39
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2015-11-26 04:21:26 -0500

    added more gulp plugins and config

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
+var args = require('yargs').argv;
+var config = require('./gulp.config')();
+var $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function() {
-    return gulp.src([
-            './src/**/*.js',
-            './*.js'
-        ])
-        .pipe(jscs())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+    log('Analyzing source with JSHint and JSCS');
+    
+    return gulp
+        .src(config.alljs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
 });
+
+///////////////////////
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
