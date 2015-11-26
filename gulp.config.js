221a2d0e1a5128142b075f45ed61180d7c1c1635
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2015-11-26 05:39:38 -0500

    add wiredep and gulp-inject

--- gulp.config.js
+++ gulp.config.js
@@ -1,16 +1,48 @@
 module.exports = function() {
-
+    var client = './src/client/';
+    var clientApp = client + 'app/';
     var config = {
+        temp: './.tmp/',
+
         /**
-         * File paths
+         * Files paths
          */
-        
-        // all js to vet
         alljs: [
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+        
+        client: client,
+        
+        index: client + 'index.html',
+        
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+        
+        less: client + 'styles/styles.less',
+        
+        /**
+         * Bower and NPM locations
+         */
+         bower: {
+             json: require('./bower.json'),
+             directory: './bower_components/',
+             ignorePath: '../..'
+         }
     };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        
+        return options;
+    } 
 
     return config;
 };
