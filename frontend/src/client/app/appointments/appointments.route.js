258739a90a226da2e7917fda613798fcdb98c6d5
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2016-05-14 16:49:20 -0700

    turned of client side booking

--- frontend/src/client/app/appointments/appointments.route.js
+++ frontend/src/client/app/appointments/appointments.route.js
@@ -10,60 +10,47 @@
     }
 
     function getStates() {
-        return [
-            // {
-            //     state: 'clientAppointments',
-            //     config: {
-            //         url: '/appointments/book',
-            //         templateUrl: 'app/appointments/appointments.html',
-            //         controller: 'ClientAppointments',
-            //         controllerAs: 'vm',
-            //         title: 'Book an Appointment',
-            //         settings: {
-            //             nav: 1,
-            //             content: 'appointments'
-            //         },
-            //     }
-            // },
-            {
-                state: 'clientAppointments_step1',
-                config: {
-                    url: '/appointments',
-                    templateUrl: 'app/appointments/appointments.step1.html',
-                    controller: 'ClientAppointmentsStepOne',
-                    controllerAs: 'vm',
-                    title: 'Book an Appointment',
-                     settings: {
-                        nav: 1,
-                        content: 'appointments'
-                    },
-                }
-            },
-            {
-                state: 'clientAppointments_step2',
-                config: {
-                    url: '/appointments/openings',
-                    templateUrl: 'app/appointments/appointments.step2.html',
-                    controller: 'ClientAppointmentsStepTwo',
-                    controllerAs: 'vm',
-                    title: 'Book an Appointment',
-                    params: {
-                        appointmentRequest: null
-                    },
-                }
-            },
-            {
-                state: 'clientAppointments_step3',
-                config: {
-                    url: '/appointments/book',
-                    templateUrl: 'app/appointments/appointments.step3.html',
-                    controller: 'ClientAppointmentsStepThree',
-                    controllerAs: 'vm',
-                    title: 'Book an Appointment',
-                    params: {
-                        appointmentRequest: null
-                    },
-                }
-            }];
+        return [];
+        // return [
+        //     {
+        //         state: 'clientAppointments_step1',
+        //         config: {
+        //             url: '/appointments',
+        //             templateUrl: 'app/appointments/appointments.step1.html',
+        //             controller: 'ClientAppointmentsStepOne',
+        //             controllerAs: 'vm',
+        //             title: 'Book an Appointment',
+        //              settings: {
+        //                 nav: 1,
+        //                 content: 'appointments'
+        //             },
+        //         }
+        //     },
+        //     {
+        //         state: 'clientAppointments_step2',
+        //         config: {
+        //             url: '/appointments/openings',
+        //             templateUrl: 'app/appointments/appointments.step2.html',
+        //             controller: 'ClientAppointmentsStepTwo',
+        //             controllerAs: 'vm',
+        //             title: 'Book an Appointment',
+        //             params: {
+        //                 appointmentRequest: null
+        //             },
+        //         }
+        //     },
+        //     {
+        //         state: 'clientAppointments_step3',
+        //         config: {
+        //             url: '/appointments/book',
+        //             templateUrl: 'app/appointments/appointments.step3.html',
+        //             controller: 'ClientAppointmentsStepThree',
+        //             controllerAs: 'vm',
+        //             title: 'Book an Appointment',
+        //             params: {
+        //                 appointmentRequest: null
+        //             },
+        //         }
+        //     }];
     }
 })();
