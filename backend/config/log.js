c51dc9f94e85ef61827bb114a2153f6049ed5242
Ryan Dodd <hotdiggitydoddo@gmail.com>
2016-06-11 22:18:17 -0700

    Updated crontask and added logging

--- backend/config/log.js
+++ backend/config/log.js
@@ -10,6 +10,23 @@
  * http://sailsjs.org/#!/documentation/concepts/Logging
  */
 
+var winston = require('winston');
+var customLogger = new winston.Logger();
+
+// A console transport logging debug and above.
+customLogger.add(winston.transports.Console, {
+  level: 'debug',
+  colorize: true
+});
+
+// A file based transport logging only errors formatted as json.
+customLogger.add(winston.transports.File, {
+  level: 'error',
+  filename: 'activity.log',
+  json: true
+});
+
+
 module.exports.log = {
 
   /***************************************************************************
@@ -24,6 +41,11 @@ module.exports.log = {
   *                                                                          *
   ***************************************************************************/
 
-  // level: 'info'
+   // Pass in our custom logger, and pass all log levels through.
+  custom: customLogger,
+  level: 'info',
+
+  // Disable captain's log so it doesn't prefix or stringify our meta data.
+  inspect: false
 
 };
